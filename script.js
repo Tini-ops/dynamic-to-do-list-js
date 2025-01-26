7c167ecdc180463f6ef2bd6e5125792465049c48
Tentswalo <tintswalototsndlovu@gmail.com>
2025-01-26 18:14:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    function addTask () {
+        const userInput = document.getElementById("task-input");
+        const taskFText = userInput.value;
+        const taskText = taskFText.trim;
+
+        if(taskText === "") {
+            alert("Please Enter a task");
+            return; 
+        } 
+        const task = document.createElement("li");
+            task.textContent = taskText;
+            const remButton = createElement('button');
+            remButton.textContent = "Remove";
+            remButton.classList.add = "remove-btn";
+            remButton.onclick = function() {
+                task.remove();
+            };
+            task.appendChild(remButton);
+            document.taskList.appendChild(task);
+            taskInput.value = "";
+
+    };
+
+    addButton.addEventListener("click", addTask);
+    taskInput.addEventListener("keypress", (event) => {
+        if(event.key === 'Enter') {
+            addTask();
+        }
+    });
+
+});
